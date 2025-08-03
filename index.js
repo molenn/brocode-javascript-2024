ecf14d022f33fb7845cb2025aa78b162622c5831
Matt Olenn <molenn@gmail.com>
2025-08-03 11:56:50 -0400

    added README.md

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log(`hello`);
-
-// window.alert(`I like pizza!`);
-
-document.getElementById("myH1").textContent = `Hello.`;
-document.getElementById("myP").textContent = `This uses JavaScript to dynamically change content.`;
-
