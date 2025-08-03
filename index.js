f0144a5c0aa8550d97e03a453f73c036ae9ffea2
Matt Olenn <molenn@gmail.com>
2025-08-03 12:18:45 -0400

    homegrown ternary operator statement

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let age = 50;
+let age = "50";
+let awesome = false;
+let myStatus = "are";
+
+awesome ? myStatus = "are" : myStatus = "are not"
 
 console.log(age);
 
-console.log(`You are ${age} years old.`)
+console.log(`You are ${age} years old and you ${myStatus} awesome.`)
+console.log(typeof age);
+console.log(awesome);
+console.log(typeof awesome);
