505b2368e76c1d67b0507d59f308839da6ee6de5
Matt Olenn <molenn@gmail.com>
2025-08-04 16:24:48 -0400

    window.prompt()

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-let fullName = "Matt Olenn";
-let age = 50;
-let student = false;
+let username; 
 
-document.getElementById("p1").textContent = `Your Name: ${fullName}`;
-document.getElementById("p2").textContent = `Your Age: ${age}`;
-document.getElementById("p3").textContent = `Student Status: ${student}`;
+username = window.prompt("What is your username?");
+
+console.log(username);
